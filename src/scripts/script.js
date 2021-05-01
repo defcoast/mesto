03d13f3269b720b9867b4a4be432d4b433b19cad
Alexander Rozhkov <al.rozhkov2222@gmail.com>
2021-05-01 22:32:41 +1000

    feat: Добавить возможность ставить лайки

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -14,6 +14,37 @@ const userBio = profile.querySelector('.profile__userbio');
 
 const newPlaceForm = document.querySelector('#new-place');
 
+const photoGridTemplate = document.querySelector('#photo-grid-template').content;
+const gridTemplateElements = document.querySelector('.photo-grid__elements');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+
 
 function openAndClosePopup() {
   popup.classList.toggle('popup_opened');
@@ -45,10 +76,8 @@ function addPlaceSubmitHandler (evt) {
   const nameInput = newPlaceForm.querySelector('[name="name"]');
   const linkInput = newPlaceForm.querySelector('[name="link"]');
 
-
   const nameInputValue = nameInput.value;
   const linkInputValue =  linkInput.value;
-
 
   openAndCloseAddPopup();
 
@@ -60,10 +89,37 @@ function addPlaceSubmitHandler (evt) {
   gridTemplateTitle.textContent = nameInputValue;
   
   gridTemplateElements.prepend(photoGridCard);
+
+  photoGridCard.querySelector('.like-btn').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('like-btn_active');
+  });
   
 }
 
+function addStartingCards() {
 
+  for (let i = 0; i < 6; i++) {
+  const photoGridCard = photoGridTemplate.querySelector('.photo-grid__element').cloneNode(true);
+  const gridTemplateImg = photoGridCard.querySelector('.photo-grid__image');
+  const gridTemplateTitle = photoGridCard.querySelector('.photo-grid__title');
+  
+  const currentItem = initialCards[i];
+
+  gridTemplateImg.src = currentItem.link;
+  gridTemplateTitle.textContent = currentItem.name
+
+  gridTemplateElements.append(photoGridCard);
+  
+  photoGridCard.querySelector('.like-btn').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('like-btn_active');
+  });
+   
+  }
+
+}
+
+
+addStartingCards();
 
 editButton.addEventListener('click', openAndClosePopup);
 closeButton.addEventListener('click', openAndClosePopup);
@@ -73,60 +129,3 @@ addButton.addEventListener('click', openAndCloseAddPopup);
 closeAddPopupButton.addEventListener('click', openAndCloseAddPopup);
 newPlaceForm.addEventListener('submit', addPlaceSubmitHandler);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//ДИНАМИЧЕСКИЕ КАРТОЧКИ
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]; 
-
-const photoGridTemplate = document.querySelector('#photo-grid-template').content;
-const gridTemplateElements = document.querySelector('.photo-grid__elements');
-
-for (let i = 0; i < 6; i++) {
-  const photoGridCard = photoGridTemplate.querySelector('.photo-grid__element').cloneNode(true);
-  const gridTemplateImg = photoGridCard.querySelector('.photo-grid__image');
-  const gridTemplateTitle = photoGridCard.querySelector('.photo-grid__title');
-
-  gridTemplateImg.src = initialCards[i].link;
-  gridTemplateTitle.textContent = initialCards[i].name
-
-  gridTemplateElements.append(photoGridCard);
-  
-}
