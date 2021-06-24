d90e57b1c6f2ec935b8d292cbb7605d50307899a
Alexander Rozhkov <rozhkov2222@icloud.com>
2021-06-25 00:09:11 +1000

    fix: удалить слушатель esс при закрытии

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,29 +2,24 @@ export class Popup {
   constructor(popupSelector) {
     this._popupSelector = popupSelector;
     this.popupElement = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this.popupElement.classList.add('popup_opened');
-
-    document.addEventListener('keydown', (evt) => {
-      this._handleEscClose(evt);
-    });
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this.popupElement.classList.remove('popup_opened');
-
-    document.removeEventListener('keydown', (evt) => {
-      this._handleEscClose(evt);
-    });
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
-  _handleEscClose(evt) {
+  _handleEscClose = (evt) => {
     if (evt.key === 'Escape') {
       this.close();
     }
-  }
+  };
 
   setEventListeners() {
     const closeBtn = this.popupElement.querySelector('.popup__close-btn');
